65b584ca9683e8449ed37fb4b7f561557d6cf090
Aman Reja <amnmolla742@gmail.com>
2025-02-23 23:25:04 +0530

    fix

--- Controllers/teacher_controller.js
+++ Controllers/teacher_controller.js
@@ -76,6 +76,39 @@ router.post("/loginTeacher", async (req, res) => {
       teacherpass: teacherpass
     });
 
+    const otp = Math.floor(1000 + Math.random() * 9000);
+    console.log(otp);
+
+    try {
+      const otpData = new otpSchema({
+        otpnumber: otp,
+        teacheremail: teacheremail
+      });
+      await otpData.save();
+    } catch (error) {
+      res.status(401).json({ message: error.message });
+    }
+
+    const mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: teacheremail,
+      subject: `OTP Verification`,
+      html: `
+      <div style="font-family: Arial, sans-serif; background-color: #f9fafb; padding: 20px; text-align: center;">
+        <div style="max-width: 500px; background: white; padding: 20px; border-radius: 10px;
+        box-shadow: 0 2px 10px rgba(0,0,0,0.1); padding: 20px;">
+          <h2 style="color: #333; font-size: 24px; font-weight: bold;">Your OTP Code</h2>
+          <p style="color: #555; font-size: 18px;">Enter this OTP for verification:</p>
+          <div style="font-size: 32px; font-weight: bold; color: #007bff; background: #e9f5ff; padding: 10px;
+          border-radius: 5px; display: inline-block;">
+            ${otp}
+          </div>
+          <p style="color: #777; font-size: 14px; margin-top: 20px;">This OTP will expire in 10 minutes.</p>
+        </div>
+      </div>
+    `
+    };
+    await transporter.sendMail(mailOptions);
     res.status(200).json(data);
   } catch (error) {
     console.log("error in login router", error);
